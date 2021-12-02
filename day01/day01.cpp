f86404162692903ec1fbfb5e935edda565ee0c69
ZachElkins <zach.elkins@gmil.com>
2021-12-02 00:14:53 -0700

    Cleaned up day01

--- day01/day01.cpp
+++ day01/day01.cpp
@@ -1,24 +1,6 @@
-#include "day01.h"
-
-int main()
-{
-    // Setup
-    std::ifstream file("input.txt");
-    std::string input;
-    std::vector<int> values;
-
-    while (file >> input) values.push_back(std::stoi(input));
-
-    // Part 1
-    int numIncreasing = numIncreasingValues(values);
-    std::cout << "Part 1: There are " << numIncreasing << " inceasing values" << std::endl;
-
-    // Part 2
-    int numWindowIncreasing = numIncreasingWindow(values, 3);
-    std::cout << "Part 2: There are " << numWindowIncreasing << " inceasing values in a window of 3" << std::endl;
-
-    return 0;
-}
+#include <iostream>
+#include <fstream>
+#include <vector>
 
 int numIncreasingValues(std::vector<int> values)
 {
@@ -52,4 +34,24 @@ int numIncreasingWindow(std::vector<int> values, int window)
     }
 
     return numIncreasing;
+}
+
+int main()
+{
+    // Setup
+    std::ifstream file("input.txt");
+    std::string input;
+    std::vector<int> values;
+
+    while (file >> input) values.push_back(std::stoi(input));
+
+    // Part 1
+    int numIncreasing = numIncreasingValues(values);
+    std::cout << "Part 1: There are " << numIncreasing << " inceasing values" << std::endl;
+
+    // Part 2
+    int numWindowIncreasing = numIncreasingWindow(values, 3);
+    std::cout << "Part 2: There are " << numWindowIncreasing << " inceasing values in a window of 3" << std::endl;
+
+    return 0;
 }
--- day01/day01.h
+++
@@ -1,5 +0,0 @@
-#include <iostream>
-#include <fstream>
-#include <vector>
-int numIncreasingValues(std::vector<int> values);
-int numIncreasingWindow(std::vector<int> values, int window);
